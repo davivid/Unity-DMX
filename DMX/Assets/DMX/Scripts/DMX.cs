2a65e14d1524fdfdcbd54a320ed0859bd02437fc
David Penney <david@dpenney.com>
2016-01-28 10:39:16 +0000

    add Editor folder

--- DMX/Assets/DMX/Scripts/DMX.cs
+++ DMX/Assets/DMX/Scripts/DMX.cs
@@ -1,7 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+using System.Collections.Generic;
+using System;
 
-public class DMX : MonoBehaviour {
+[ExecuteInEditMode]
+public class DMX : MonoBehaviour 
+{
+	static DMX instance = null;
+
+	//DMX USB PRO API
+	private const byte DMX_PRO_HEADER_SIZE = 4;
+	private const byte DMX_PRO_START_MSG = 0x7E;
+	private const byte DMX_PRO_LABEL_DMX = 6;
+	private const byte DMX_PRO_LABEL_SERIAL_NUMBER = 10;
+	private const byte DMX_PRO_START_CODE = 0;
+	private const byte DMX_PRO_START_CODE_SIZE = 1;
+	private const byte DMX_PRO_END_MSG = 0xE7;
+	private const byte DMX_PRO_END_MSG_SIZE = 1;
+	//
+
+	private static SerialPort serialPort;
+	
+	public List<string> serial_ports;
+	public int serial_port_idx;
+	public string deviceSerialNumber;
+	public string deviceName;
+	
+	private int nChannels;
+	private byte[] levels;
+	private string[] labels;
+	private byte[] txPacket, rxPacket;
+	private int rxPacket_idx;
+	private bool rxStarted, rxEnded;
+	
+	private bool sendDMX = false;
+
 
 	// Use this for initialization
 	void Start () {
