d06ef57104519afc969ef0107124b055443b28ab
David Penney <david@dpenney.com>
2016-01-28 16:22:24 +0000

    add DMX code + Custom Inspector

--- DMX/Assets/DMX/Scripts/DMX.cs
+++ DMX/Assets/DMX/Scripts/DMX.cs
@@ -7,11 +7,9 @@ using System.Collections.Generic;
 using System;
 
 [ExecuteInEditMode]
-public class DMX : MonoBehaviour 
+public class DMX : MonoBehaviour
 {
-	static DMX instance = null;
-
-	//DMX USB PRO API
+	//DMX USB Pro API
 	private const byte DMX_PRO_HEADER_SIZE = 4;
 	private const byte DMX_PRO_START_MSG = 0x7E;
 	private const byte DMX_PRO_LABEL_DMX = 6;
@@ -21,7 +19,7 @@ public class DMX : MonoBehaviour
 	private const byte DMX_PRO_END_MSG = 0xE7;
 	private const byte DMX_PRO_END_MSG_SIZE = 1;
 	//
-
+	
 	private static SerialPort serialPort;
 	
 	public List<string> serial_ports;
@@ -29,23 +27,191 @@ public class DMX : MonoBehaviour
 	public string deviceSerialNumber;
 	public string deviceName;
 	
-	private int nChannels;
-	private byte[] levels;
-	private string[] labels;
-	private byte[] txPacket, rxPacket;
+	public int nChannels = 24;
+	public byte[] levels;
+	public string[] labels;
+	private byte[] rxPacket;
 	private int rxPacket_idx;
 	private bool rxStarted, rxEnded;
 	
-	private bool sendDMX = false;
-
-
-	// Use this for initialization
-	void Start () {
+	public bool sendDMX = false;
 	
+	public byte[] txPacket;
+	
+	void Start()
+	{	
+		
+		rxPacket = new byte[600];
+		setChannels();
+		
+		GetPortNames();
+
+		if (deviceName != "") OpenConnection();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	public void setChannels()
+	{
+		levels = new byte[nChannels];
+		labels = new string[nChannels];
+	}
 	
+	public void setLevel(int channel, int val)
+	{
+		int c = channel-1;
+		
+		if (c >= 0 && c < nChannels)
+		{
+			levels[c] = (byte)Mathf.Clamp(val,0,255);
+		}
+		
+		sendDMX = true;
+	}
+
+	public void setLabel(int channel, string label)
+	{
+		int c = channel-1;
+		
+		if (c >= 0 && c < nChannels)
+		{
+			labels[c] = label;
+		}
+	}	
+	
+	void SendPacket(byte label, byte[] data)
+	{
+		if (serialPort == null || !serialPort.IsOpen) return;
+
+		int dataSize = data.Length;
+		if (dataSize > 0) dataSize += DMX_PRO_START_CODE_SIZE;
+		
+		txPacket = new byte[DMX_PRO_HEADER_SIZE + dataSize + DMX_PRO_END_MSG_SIZE];
+
+		txPacket[0] = DMX_PRO_START_MSG;
+		txPacket[1] = label;
+		txPacket[2] = (byte)(dataSize & 255);
+		txPacket[3] = (byte)((dataSize >> 8) & 255);
+		
+		if (dataSize > 0)
+		{
+			txPacket[4] = DMX_PRO_START_CODE;
+			
+			for(int i = 0; i < dataSize-1; i++)
+			{
+				txPacket[DMX_PRO_HEADER_SIZE+1 + i] = data[i];  
+			}
+		}
+		
+		txPacket[DMX_PRO_HEADER_SIZE + dataSize] = DMX_PRO_END_MSG;
+		
+		serialPort.Write(txPacket, 0, txPacket.Length);
+	}
+	
+	void ProcessSerialNumber(byte[] data)
+	{	
+		deviceSerialNumber = data[3].ToString("X") + data[2].ToString("X") + data[1].ToString("X") + data[0].ToString("X");
+	}
+	
+	void ProcessRxPacket()
+	{
+		int label = rxPacket[1];
+		int dataSizeLSB = rxPacket[2];
+		int dataSizeMSB = rxPacket[3];
+		
+		byte[] data = new byte[dataSizeLSB];
+		Buffer.BlockCopy(rxPacket,DMX_PRO_HEADER_SIZE,data,0,dataSizeLSB);
+		
+		switch (label)
+		{
+			case DMX_PRO_LABEL_SERIAL_NUMBER:
+				ProcessSerialNumber(data);
+			break;
+		}
+	}
+	
+	void Update()
+	{
+		if (serialPort == null || !serialPort.IsOpen) return;
+		
+		if (sendDMX) 
+		{
+			sendDMX = false;
+			SendPacket(DMX_PRO_LABEL_DMX, levels);
+		}
+		
+		while (serialPort.BytesToRead > 0)
+		{
+			byte inByte = (byte)serialPort.ReadByte();
+			
+			if (inByte == DMX_PRO_START_MSG)
+			{
+				rxPacket_idx = 0;
+				rxStarted = true;
+				rxEnded = false;
+				
+				rxPacket[rxPacket_idx++] = inByte;
+			}
+			
+			else if (inByte == DMX_PRO_END_MSG)
+			{
+				rxEnded = true;
+				if (rxStarted && rxEnded) ProcessRxPacket();
+			}
+			
+			else if (rxStarted) 
+			{
+				rxPacket[rxPacket_idx++] = inByte;
+			}
+			
+			//TODO: if (Timeout) return;
+		}
+	}
+	
+	void GetPortNames()
+	{
+		int p = (int)System.Environment.OSVersion.Platform;
+		serial_ports = new List<string>();
+		serial_ports.Add("");
+		
+		if(p == 4 || p == 128 || p == 6)
+		{
+			string[] ttys = Directory.GetFiles("/dev/", "tty.*");
+			foreach(string dev in ttys)
+			{
+				serial_ports.Add(dev.Replace("/", "\\")); //Replace forward slash to play nicely with gui.		
+			}
+		}
+	}
+	
+	public void OpenConnection()
+	{
+		if (serialPort != null) { serialPort.Close(); serialPort.Dispose(); }
+
+		serialPort = new SerialPort(serial_ports[serial_port_idx].Replace("\\", "/"), 57600, Parity.None, 8, StopBits.One);
+		
+		try 
+		{
+			serialPort.Open();
+			serialPort.ReadTimeout = 50;
+			deviceName = serial_ports[serial_port_idx].Replace("\\", "/");
+			SendPacket(DMX_PRO_LABEL_SERIAL_NUMBER, new byte[0]);
+		}
+		catch (System.Exception e) 
+		{
+			deviceSerialNumber = "";
+			deviceName = "";
+			Debug.LogException(e);
+		}
+	}
+	
+	void OnApplicationQuit()
+	{
+		for (int i =0; i < nChannels; i++)
+		{
+			levels[i] = 0;
+		}
+
+		SendPacket(DMX_PRO_LABEL_DMX, levels);
+
+		if (serialPort != null) serialPort.Close();
 	}
 }
---
+++ DMX/Assets/Editor/DMXInspector.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[ExecuteInEditMode]
+[CustomEditor(typeof(DMX))]
+public class DMXInspector : Editor {
+	
+	public override void OnInspectorGUI()
+	{
+		var script = (DMX) target;
+		EditorUtility.SetDirty( target );
+		
+		//Device ID
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.PrefixLabel("Device s/n: ");
+		EditorGUILayout.LabelField(script.deviceSerialNumber);
+		EditorGUILayout.EndHorizontal();
+		
+		//Device
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.PrefixLabel("Device name: ");
+		
+		script.deviceName =  EditorGUILayout.TextField(script.deviceName.Substring(script.deviceName.LastIndexOf('.') + 1));
+		EditorGUILayout.EndHorizontal();
+		
+		//Select Device
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.PrefixLabel("Serial ports: ");
+ 		int i = EditorGUILayout.Popup(script.serial_port_idx, script.serial_ports.ToArray());
+		if (i != script.serial_port_idx)
+		{
+			script.serial_port_idx = i;
+			script.OpenConnection();
+		}
+		EditorGUILayout.EndHorizontal();
+		
+		//Number of channels
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.PrefixLabel("Number Channels: ");
+		i = EditorGUILayout.IntField(script.nChannels);
+		{
+			i = Mathf.Clamp(i, 24, 1024);
+			if (i != script.nChannels)
+			{
+				script.nChannels = i;
+				script.setChannels();
+			}
+		}
+		EditorGUILayout.EndHorizontal();
+
+	}
+}
