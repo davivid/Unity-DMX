60876a0f830ef7264266461eeb82a02d00a274b3
David Penney <david@dpenney.com>
2016-01-30 13:43:39 +0000

    Add Example

---
+++ DMX/Assets/DMX/Scripts/Example.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+
+	public GameObject dmxObject;
+	DP.DMX dmx;
+
+	// Use this for initialization
+	void Start () 
+	{
+		dmxObject = GameObject.Find("DMXObject");
+		dmx = dmxObject.GetComponent<DP.DMX>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Set Value
+		dmx[1] = (byte)(Mathf.PingPong(Time.time*100, 255));
+
+		//Get Value
+		dmx[3] = dmx[1];
+	}
+}
