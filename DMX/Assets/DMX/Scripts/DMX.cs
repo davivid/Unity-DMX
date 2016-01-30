e8a9eb5782cb9892629faa131e6b697ac8d9e1a4
David Penney <david@dpenney.com>
2016-01-30 14:54:34 +0000

    Add comments

--- DMX/Assets/DMX/Scripts/DMX.cs
+++ DMX/Assets/DMX/Scripts/DMX.cs
@@ -8,18 +8,30 @@ using System;
 
 namespace DP
 {
-[ExecuteInEditMode]
-public class DMX : MonoBehaviour {
+	/// <summary>
+	/// DMX Interface that targets the Enttec DMX USB Pro Mk2 and Clones.
+	/// </summary>
+	/// <remarks>
+	/// Author: David Penney 2016.
+	/// 
+	/// Notes:
+	///   1. Must set Player Settings | Optimisations | API Compatibility Level to ".NET 2.0" which enables Serial I/O
+	///
+	/// </remarks>
 
-	private const int DMX_INDEX_OFFSET = 5;
-	private const int DMX_MESSAGE_OVERHEAD = 6;
+[ExecuteInEditMode]
+public class DMX : MonoBehaviour 
+{
 
 	private const int N_DMX_CHANNELS = 512;
-	public int nChannels
-	{
-		get {return N_DMX_CHANNELS;}
-	}
+	public int nChannels {get {return N_DMX_CHANNELS;}}
 
+	#region --- DMX PRO API
+	/// <summary>
+	/// DMX PRO API
+	/// https://www.enttec.com/docs/dmx_usb_pro_api_spec.pdf
+	///	The Mk2 API is available upon request to Enttec
+	/// </summary>
 	private const byte DMX_PRO_HEADER_SIZE = 4;
 	private const byte DMX_PRO_START_MSG = 0x7E;
 	private const byte DMX_PRO_LABEL_DMX = 6;
@@ -28,32 +40,72 @@ public class DMX : MonoBehaviour {
 	private const byte DMX_PRO_START_CODE_SIZE = 1;
 	private const byte DMX_PRO_END_MSG = 0xE7;
 	private const byte DMX_PRO_END_MSG_SIZE = 1;
+	//
+	private const int DMX_PRO_DATA_INDEX_OFFSET = 5;
+	private const int DMX_PRO_MESSAGE_OVERHEAD = 6;
+	#endregion
 
-
-	private const int TX_BUFFER_LENGTH = DMX_MESSAGE_OVERHEAD + N_DMX_CHANNELS;
+	/// <summary>
+	/// Thread that handles the serial I/O.
+	/// </summary>
 	private Thread dmxThread;
+
+	/// <summary>
+	/// Flag to indicate Levels have been updated and DMX needs to be sent
+	/// </summary>
 	private bool updateDMX;
 
+	/// <summary>
+	/// The instance of the serial port used to communicate with the DMX controller.
+	/// </summary>
 	private static SerialPort serialPort;
+
+	/// <summary>
+	/// List containing strings of all available serial ports
+	/// </summary>
 	public List<string> serialPorts;
+
+	/// <summary>
+	/// Index of the selected serial port
+	/// </summary>
 	public int serialPortIdx;
 
+	/// <summary>
+	/// Buffer containg level of all DMX channels.
+	/// </summary>
 	private byte[] DMXLevels = new byte[N_DMX_CHANNELS];
-	private byte[] TxBuffer = new byte[DMX_MESSAGE_OVERHEAD + N_DMX_CHANNELS];
+
+	/// <summary>
+	/// DMX message buffer length.
+	/// </summary>
+	private const int TX_BUFFER_LENGTH = DMX_PRO_MESSAGE_OVERHEAD + N_DMX_CHANNELS;
+
+	/// <summary>
+	/// DMX message buffer.
+	/// </summary>
+	private byte[] TxBuffer = new byte[DMX_PRO_MESSAGE_OVERHEAD + N_DMX_CHANNELS];
 
 	void Start () 
 	{	
+
+		//List avialable serial ports and connected to last selected.
 		GetPortNames();
 		if (serialPortIdx > 0) OpenSerialPort();
 
+		//Init the TX Buffer
 		initTXBuffer();
 
+		//Start the serial io thread
 		dmxThread = new Thread(ThreadedIO);
 		dmxThread.Start();
 
+		//Flag to send default DMX values
 		updateDMX = true;
 	}
 
+	/// <summary>
+	/// Gets of sets the DMX level at specified channel
+	/// </summary>
 	public int this[int index]
 	{
 		get
@@ -85,6 +137,9 @@ public class DMX : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// The Threaded function that processes the serial i/o.
+	/// </summary>
 	private void ThreadedIO()
 	{
 	Debug.Log("Thread Start");
@@ -93,14 +148,18 @@ public class DMX : MonoBehaviour {
 			if (updateDMX)
 			{
 				updateDMX = false;
-				Buffer.BlockCopy(DMXLevels,0,TxBuffer,DMX_INDEX_OFFSET,N_DMX_CHANNELS);
+				Buffer.BlockCopy(DMXLevels,0,TxBuffer,DMX_PRO_DATA_INDEX_OFFSET,N_DMX_CHANNELS);
 				if (serialPort != null && serialPort.IsOpen) {serialPort.Write(TxBuffer, 0, TX_BUFFER_LENGTH); };
 			}
 
+			//TODO: Recieve Serial
 			//if (serialPort.BytesToRead > 0)
 		}
 	}
 
+	/// <summary>
+	/// List the avilable serial ports
+	/// </summary>
 	private void GetPortNames()
 	{
 		int p = (int)System.Environment.OSVersion.Platform;
@@ -117,6 +176,9 @@ public class DMX : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// Open the selected serial port
+	/// </summary>
 	public void OpenSerialPort()
 	{
 		if (serialPort != null)
@@ -140,6 +202,9 @@ public class DMX : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// Init the TxBufffer with Header and End bytes
+	/// </summary>
 	private void initTXBuffer()
 	{
 		for(int i=0; i<TX_BUFFER_LENGTH; i++) TxBuffer[i] = (byte)255;
@@ -152,6 +217,9 @@ public class DMX : MonoBehaviour {
 		TxBuffer[517] = DMX_PRO_END_MSG;
 	}
 
+	/// <summary>
+	/// Shutdown DMX and cleanup.
+	/// </summary>
 	void OnApplicationQuit()
 	{
 		//Reset DMX levels to 0;
